3476e04bb9f309e150c9035485ba624adef074b1
Kaj Rietberg <kaj@kajrietberg.nl>
2019-04-03 14:57:04 +0200

    Functions aanpassingen

--- src/lambda/fetch-rwd-api.js
+++ src/lambda/fetch-rwd-api.js
@@ -4,16 +4,19 @@ require('dotenv').config();
 const AUTH = process.env.NETLIFY_NUMBERPLATE;
 const API_ENDPOINT =
     `https://opendata.rdw.nl/resource/m9d7-ebf2.json?kenteken=${AUTH}`;
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type'
+}
 
 exports.handler = async (event, context) => {
     return fetch(API_ENDPOINT)
-        .then(response => response.json())
+        .then(response => response.json() )
         .then(data => ({
             statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: `${data[0].merk} ${data[0].handelsbenaming} *BA DUM TSSS*`
+            headers,
+            body: JSON.stringify(data)
         }))
+
         .catch(error => ({ statusCode: 422, body: String(error) }));
 };
